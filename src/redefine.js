d29320613398852cac72b1bd82b265cd171e5810
WebReflection <andrea.giammarchi@gmail.com>
2013-01-22 20:40:51 -0800

    Auto-update tests

--- src/redefine.js
+++ src/redefine.js
@@ -208,12 +208,12 @@ var _ = this._ = function(_, Function, Object) {
     // but at least it's consistent ^_^
     // Opera Mobile or all other browsers
     // won't be affected
-    create(redefine({},{_:later(Object)}))._;
-    /*
+    //create(redefine({},{_:later(Object)}))._;
+    //*
     create(defineProperty({},"_",{get:function(){
       defineProperty(this,"_",{value:"_"});
     }}))._
-    */
+    //*/
   } catch(o_O) {
     clear(nullObject);
     hasDescriptorBug = true;
